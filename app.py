bbc0ba7001af484f16afdd9e1dcf90189a5c6c0a
Sumon Saha <sumongenex2026@gmail.com>
2026-05-18 09:24:19 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ from PIL import Image, ImageFilter
 import os
 import uuid
 import io
+import asyncio
+import gc
+
+semaphore = asyncio.Semaphore(1)
+
+MAX_SIZE = 1200
 
 # =========================
 # APP SETUP
@@ -50,36 +56,39 @@ def load_model():
 
 @app.post("/remove-bg")
 async def remove_bg(file: UploadFile = File(...)):
-
-    file_id = str(uuid.uuid4())
-    output_path = f"{OUTPUT_FOLDER}/{file_id}.png"
-
-    input_bytes = await file.read()
-    image = Image.open(io.BytesIO(input_bytes)).convert("RGBA")
-
-    # upscale before processing
-    image = image.resize((image.width * 2, image.height * 2))
-
-    output_image = remove(
-        image,
-        session=session,
-        alpha_matting=False
-    )
-
-    # downscale back
-    output_image = output_image.resize(
-        (output_image.width // 2, output_image.height // 2)
-    )
-
-    # smooth edges
-    alpha = output_image.getchannel("A")
-    alpha = alpha.filter(ImageFilter.GaussianBlur(1))
-    output_image.putalpha(alpha)
-
-    output_image.save(output_path)
-
-    return FileResponse(
-        output_path,
-        media_type="image/png",
-        filename="output.png"
-    )
+    async with semaphore:
+        file_id = str(uuid.uuid4())
+        output_path = f"{OUTPUT_FOLDER}/{file_id}.png"
+    
+        input_bytes = await file.read()
+        image = Image.open(io.BytesIO(input_bytes)).convert("RGBA")
+    
+        # upscale before processing
+        # image = image.resize((image.width * 2, image.height * 2))
+    
+        # Downscale large images
+            image.thumbnail((MAX_SIZE, MAX_SIZE))
+    
+        output_image = remove(
+            image,
+            session=session,
+            alpha_matting=False
+        )
+    
+        # downscale back
+        # output_image = output_image.resize(
+        #     (output_image.width // 2, output_image.height // 2)
+        # )
+    
+        # smooth edges
+        alpha = output_image.getchannel("A")
+        alpha = alpha.filter(ImageFilter.GaussianBlur(1))
+        output_image.putalpha(alpha)
+    
+        output_image.save(output_path)
+    
+        return FileResponse(
+            output_path,
+            media_type="image/png",
+            filename="output.png"
+        )
