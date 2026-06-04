cfc5d6085794fb2e4387b37f7f4681534053df7b
Sumon Saha <sumongenex2026@gmail.com>
2026-06-04 11:39:15 +0600

    Update app.py

--- app.py
+++ app.py
@@ -25,12 +25,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://snazzy-snickerdoodle-8d6c74.netlify.app",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # =========================
 # STORAGE (Railway-safe)
 # =========================
